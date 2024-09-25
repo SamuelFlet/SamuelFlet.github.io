9bd1c594527e8cbd3380c6b4b932b1aafae8f1fb
Samuel Fletcher <49329336+SamuelFlet@users.noreply.github.com>
2024-09-25 12:54:47 -0400

    added so so much

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./src/**/*.{html,js}", "./index.html"],
   theme: {
-    extend: {},
+    fontWeight: {
+      thin: "100",
+      extralight: "200",
+      light: "300",
+      normal: "400",
+      medium: "500",
+      semibold: "600",
+      bold: "700",
+      extrabold: "800",
+      black: "900",
+    },
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        cormorant: ["Cormorant Garamond", "serif"],
+      },
+    },
   },
   plugins: [],
-}
+};
